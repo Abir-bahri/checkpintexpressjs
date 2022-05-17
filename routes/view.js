baf0c3eff4af118390a3d87bc403836578be7661
Abir-bahri <bahriabir15@gmail.com>
2022-05-17 20:49:13 +0200

    first commit

--- routes/view.js
+++ routes/view.js
@@ -2,14 +2,28 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-const timemiddleware = (req,res,next)=>{
-//     if (Date.now()<9 || Date.now()> 17) {
+// const timemiddleware = (req,res,next)=>{
+// //     if (Date.now()<9 || Date.now()> 17) {
 
-//         console.log('the site is closed')
-// }else{ 
+// //         console.log('the site is closed')
+// // }else{ 
     
-    console.log('Time:', Date.now());
-    next();
+//     console.log('Time:', Date.now());
+//     next();
+// }
+const hours=(new Date().getHours())
+//Middleware
+const timemiddleware = (req, res, next)=> {
+    if (hours>=12 && hours<=17){
+         console.log('we are opened')
+
+         next();
+    }
+
+    else {console.log('we are closed');
+    res.sendFile(path.join(__dirname,'../','Public','close.html'))
+
+    }
 }
 
 router.get('/',timemiddleware,(req,res)=>{
